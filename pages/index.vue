ce5d81d1690d817706eba34d4133c2cef8d52e7e
Roman Orlovskiy <irom-io@ya.ru>
2025-09-29 17:13:04 +0300

    Init nuxt

---
+++ pages/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h1>Добро пожаловать в Brand Games!</h1>
+    <p>Это простой Nuxt проект.</p>
+  </div>
+</template>
+
+<script setup>
+// Здесь можно добавить логику компонента
+</script>
+
+<style scoped>
+h1 {
+  color: #2c3e50;
+  text-align: center;
+  margin-top: 2rem;
+}
+
+p {
+  text-align: center;
+  color: #7f8c8d;
+  font-size: 1.2rem;
+}
+</style>
