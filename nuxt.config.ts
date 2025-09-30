0f64186fc810e8dc5ac9b93471d150b81fcb22b3
Roman Orlovskiy <irom-io@ya.ru>
2025-09-30 12:24:52 +0300

    Fix nuxt

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  devServer: {
+    host: '0.0.0.0',
+    port: 3000
+  },
   css: [
     'vuetify/lib/styles/main.sass',
     '@mdi/font/css/materialdesignicons.css'
