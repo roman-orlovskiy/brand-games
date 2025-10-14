9981add45732cd79a6c5b1fc89462265857e6d82
Roman Orlovskiy <irom-io@ya.ru>
2025-10-14 18:08:56 +0300

    Add server 0 0 0 0

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,11 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
 
+  devServer: {
+    host: '0.0.0.0', // Доступ по локальной сети
+    port: 3000
+  },
+
   modules: [
     '@pinia/nuxt',
     '@nuxt/image',
