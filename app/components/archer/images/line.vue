e091146446298d6efde4b556a36df7ef6547ae6f
Roman Orlovskiy <irom-io@ya.ru>
2025-10-20 00:05:04 +0300

    Fix line

--- app/components/archer/images/line.vue
+++ app/components/archer/images/line.vue
@@ -1,6 +1,14 @@
 <template>
   <svg id="line" ref="svgRef" viewBox="0 0 1200 700" fill="none" xmlns="http://www.w3.org/2000/svg">
-    <path ref="pathRef" :d="pathData" :stroke="trajectoryColor" :stroke-width="strokeWidth" :stroke-dasharray="strokeDasharray"/>
+    <defs>
+      <linearGradient id="trajectoryGradient" x1="0%" y1="0%" x2="100%" y2="0%">
+        <stop offset="0%" :stop-color="trajectoryColor" stop-opacity="1"/>
+        <stop offset="16%" :stop-color="trajectoryColor" stop-opacity="1"/>
+        <stop offset="16%" :stop-color="trajectoryColor" stop-opacity="0"/>
+        <stop offset="100%" :stop-color="trajectoryColor" stop-opacity="0"/>
+      </linearGradient>
+    </defs>
+    <path ref="pathRef" :d="pathData" stroke="url(#trajectoryGradient)" :stroke-width="strokeWidth" :stroke-dasharray="strokeDasharray"/>
   </svg>
 </template>
 
