22f24c8887e1b7389b7907dbb68b006c499babc4
Roman Orlovskiy <irom-io@ya.ru>
2025-10-29 10:14:50 +0300

    Add archer

---
+++ app/pages/archer.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="archer-page">
+    <div class="archer-page__container">
+      <ArcherGame class="archer-page__game" />
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+definePageMeta({
+  layout: false,
+  title: 'Игра Лучник'
+})
+</script>
+
+<style scoped lang="scss">
+.archer-page {
+  min-height: 100vh;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+
+  &__container {
+    flex: 1;
+    width: 100%;
+    display: flex;
+  }
+
+  &__game {
+    width: 100%;
+    height: 100%;
+  }
+}
+</style>
+
+
